d39013fa1363c7c668cb99eb4cd6274788904618
tuck1s <steve@thetucks.com>
2023-11-27 22:52:45 +0000

    Add server,port,user,pass options

--- smtp-traffic-gen.py
+++ smtp-traffic-gen.py
@@ -18,11 +18,16 @@ def eprint(*args, **kwargs):
 # -----------------------------------------------------------------------------
 # async SMTP email sending
 # -----------------------------------------------------------------------------
-async def send_msgs_async(msgs: list, host='localhost', port=25, snooze = 0.0):
+async def send_msgs_async(msgs: list, host='localhost', port=25, snooze = 0.0, username=None, password=None):
     try:
         # Don't attempt SSL from start of connection, but allow STARTTLS (default) with loose certs
         smtp = SMTP(hostname=host, port=port, use_tls=False, validate_certs=False)
         await smtp.connect()
+        if username and password:
+            code, msg = await smtp.auth_login(username, password)
+            if code <200 or code >299:
+                raise(code, msg)
+
         for msg in msgs:
             t1 = time.perf_counter()
             errors, etext = await smtp.send_message(msg)
@@ -86,6 +91,9 @@ if __name__ == "__main__":
     parser.add_argument('--yahoo-backoff', type=float, help='Yahoo-specific bounce rates to cause backoff mode')
     parser.add_argument('--max-connections', type=int, default=20, help='Maximum number of SMTP connections to open')
     parser.add_argument('--duration', type=int, default = 0, help='duration to cadence this send, default is "as fast as possible"')
+    parser.add_argument('--server', type=str, default = 'localhost:25', help='server:port to inject messages to')
+    parser.add_argument('--auth-user', type=str, help='authentication user name')
+    parser.add_argument('--auth-pass', type=str, help='authentication password')
     args = parser.parse_args()
     bounces = BounceCollection(args.bounces, args.yahoo_backoff)
     content = EmailContent(args.sender_subjects, args.html_content, args.txt_content)
@@ -106,17 +114,23 @@ if __name__ == "__main__":
         snooze = 0
     # port 2525 direct to the sink
     # port 25   queue_to_sink listener (passes messages through the MTA to show stats etc)
+    host, port = args.server.split(':')
+    if port == '':
+        port = 25
     mail_params = {
-        'host': 'localhost',
-        'port': 25,
+        'host': host,
+        'port': port,
         'messages_per_connection': 100,
         'max_connections': args.max_connections,
         'snooze': snooze,
+        'username': args.auth_user,
+        'password': args.auth_pass,
     }
 
     start_time = time.time()
     msgs = rand_messages(batch_size, names, content, bounces)
-    print('Sending {} emails over max {} SMTP connections, {} max messages per connection, cadence {:0.4f} seconds per mail'
-        .format(batch_size, mail_params['max_connections'], mail_params['messages_per_connection'], mail_params['snooze']))
+    print('Sending {} emails over max {} SMTP connections to {}:{}, {} max messages per connection, cadence {:0.4f} seconds per mail'
+        .format(batch_size, mail_params['max_connections'], mail_params['host'], mail_params['port'], mail_params['messages_per_connection'], mail_params['snooze']))
+    print('auth-user: {}, auth-pass: {}'.format(args.auth_user, args.auth_pass))
     asyncio.run(send_batch(msgs, **mail_params))
     print('Done in {0:.1f}s.'.format(time.time() - start_time))
